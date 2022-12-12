d263d29d12a17f6188838e1b132ec3c8225b7f24
lawrencethomp <lawrencethomp@gmail.com>
2022-12-12 14:37:33 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ require('chromedriver');
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
-    await driver.get('http://automationpractice.com/index.php');
+    await driver.get('https://magento.softwaretestingboard.com/');
     // await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
     // await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
   } finally {
