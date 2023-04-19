295c7bb3abb05cefc8fb99880e396e9142330ece
lawrencethomp <lawrencethomp@gmail.com>
2023-04-19 10:44:09 -0400

    Create locators.js

---
+++ locators.js
@@ -0,0 +1,4 @@
+
+  // Default message = class "not-logged-in"
+  // "Sign in" text = class authorization-link
+  // "Create an Account" - Link: https://magento.softwaretestingboard.com/customer/account/create/
