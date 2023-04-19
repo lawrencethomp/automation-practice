5dbae78d6844ed4b42f8ba2505063b3620e346fd
lawrencethomp <lawrencethomp@gmail.com>
2023-04-19 17:02:28 -0400

    Create urls.js

---
+++ test/conf/urls.js
@@ -0,0 +1,4 @@
+const mainPageUrl = "https://magento.softwaretestingboard.com/";
+const whatsNewUrl = "https://magento.softwaretestingboard.com/what-is-new.html";
+const loginPageUrl = "https://magento.softwaretestingboard.com/customer/account/login/";
+const registerPageUrl = "https://magento.softwaretestingboard.com/customer/account/create/";
