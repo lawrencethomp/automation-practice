643fa33fe8d3cfe96a2b46245eaed5ebe59745f2
lawrencethomp <lawrencethomp@gmail.com>
2023-04-19 17:04:01 -0400

    Update urls.js

--- test/conf/urls.js
+++ test/conf/urls.js
@@ -2,3 +2,5 @@ const mainPageUrl = "https://magento.softwaretestingboard.com/";
 const whatsNewUrl = "https://magento.softwaretestingboard.com/what-is-new.html";
 const loginPageUrl = "https://magento.softwaretestingboard.com/customer/account/login/";
 const registerPageUrl = "https://magento.softwaretestingboard.com/customer/account/create/";
+const womenPageUrl = "https://magento.softwaretestingboard.com/women.html";
+const menPageUrl = "https://magento.softwaretestingboard.com/men.html";
