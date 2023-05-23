367e3510d96f77920583a0f738283c47c20d340d
lawrencethomp <lawrencethomp@gmail.com>
2023-05-23 17:35:04 -0400

    Cleaned up imports on page

--- pages/homepage.js
+++ pages/homepage.js
@@ -1,18 +1,8 @@
-const {Builder, Browser, Key} = require('selenium-webdriver');
-const pageURLs = require('../conf/urls');
-const baseSiteUrl = pageURLs.mainPageUrl;
-
 const getTextByClass  = require("../pageutils");
-const { getWebSite, closeWebSite } = require("../utils/pageactions");
 
 async function checkDefaultWelcomeMsg() {
-    try {
-      // await getWebSite(baseSiteUrl);
       var notLoggedInText = await getTextByClass('not-logged-in');
-    } finally {
-      // await closeWebSite();
       return notLoggedInText;
-    }
 }
 
 module.exports = checkDefaultWelcomeMsg;
