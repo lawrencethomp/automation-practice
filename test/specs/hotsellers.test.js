fbb79881ef081056494dc76b708c195d828cfd5a
lawrencethomp <lawrencethomp@gmail.com>
2024-02-19 19:08:40 -0500

    Create hotsellers.test.js

---
+++ test/specs/hotsellers.test.js
@@ -0,0 +1,22 @@
+// Grid will have items appear
+
+// Hot Sellers items have 
+  // sizes
+    // size class: swatch-attribute-size
+  // colors
+    // color class: swatch-attribute-color
+  // rating
+    // class: rating-result
+  // link to reviews
+  // onHover will be highlighted
+
+// Navigate to Homepage ✔️
+  //   beforeAll(() => {
+  //     homepage.goToURL("https://magento.softwaretestingboard.com");
+  //   });
+// Scroll to Content grid 
+  // let element = driver.findElement(By.className('footer'));
+  // driver.executeScript("arguments[0].scrollIntoView(true);", element);
+// Verify that the grid is visible
+  // boolean t = driver.findElement(By.name("txt-val")).isDisplayed();
+// We need to create that something is true in testutils.js
